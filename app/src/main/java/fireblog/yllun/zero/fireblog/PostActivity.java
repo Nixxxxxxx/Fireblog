43d521c38f2f5af286ee651698f4fbfdebac93b6
Nissen Parianen <n.parianen@gmail.com>
2017-01-30 18:11:27 +0100

    Added button show selected image and upload to storage

--- app/src/main/java/fireblog/yllun/zero/fireblog/PostActivity.java
+++ app/src/main/java/fireblog/yllun/zero/fireblog/PostActivity.java
@@ -1,19 +1,97 @@
 package fireblog.yllun.zero.fireblog;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton mSelectImage;
+    private EditText mPostTitle;
+    private EditText mPostMesage;
+    private Button mBtSubmit;
+    private  Uri mImageUri = null;
+    private ProgressDialog mProgressDialog;
+
+    private static final int  GALERY_REQUEST = 1;
+    private StorageReference mStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mProgressDialog = new ProgressDialog(this);
+
+        mSelectImage = (ImageButton) findViewById(R.id.imageButton);
+
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galeryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galeryIntent.setType("image/*");
+                startActivityForResult(galeryIntent,GALERY_REQUEST);
+            }
+        });
+
+        mPostTitle = (EditText)findViewById(R.id.idEditTitle);
+        mPostMesage = (EditText)findViewById(R.id.idEditMesage);
+
+        mBtSubmit = (Button) findViewById(R.id.idBtSubmit);
+        mBtSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPosting();
+            }
+        });
+
     }
 
+
+    private void startPosting(){
+        mProgressDialog.setMessage("Posting to blog");
+        mProgressDialog.show();
+        String title = mPostTitle.getText().toString().trim();
+        String message = mPostMesage.getText().toString().trim();
+        if(!TextUtils.isEmpty(title) && !TextUtils.isEmpty(message) && mImageUri != null){
+
+            StorageReference filepath = mStorage.child("Blog_Image").child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    mProgressDialog.dismiss();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALERY_REQUEST && resultCode == RESULT_OK){
+            mImageUri = data.getData();
+            mSelectImage.setImageURI(mImageUri);
+        }
+    }
 }
+
+
+
+
